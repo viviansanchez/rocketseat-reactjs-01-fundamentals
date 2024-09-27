6173e4ccd84a7d288dfe072b86bc1fc21f07bc06
Vivian Sanchez Desktop <visasal88@gmail.com>
2024-09-27 14:08:51 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <div>
+      <Post author="Vivian" content="blablagksjgfksjfksJFsj" />
+      <Post author="Jorge" content="aaaaaaaaa" />
+    </div>
+  );
+}
