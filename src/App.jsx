f47d20b7a02bffdc073c8e9772c10fa47ee0c889
Vivian Laptop Nitro <visasal88@gmail.com>
2024-10-09 08:46:56 -0300

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+import "./styles.css";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post author="Vivian" content="blablagksjgfksjfksJFsj" />
       <Post author="Jorge" content="aaaaaaaaa" />
     </div>
