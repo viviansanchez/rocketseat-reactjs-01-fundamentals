fd56883578734dec471db55a6b6c09b03316998e
Vivian Sanchez Desktop <visasal88@gmail.com>
2024-10-11 14:43:09 -0300

    refactor: implement Avatar component, removed imgs and unneeded styles

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/diego3g.png" />
+      <Avatar hasBorder={false} src="https://github.com/viviansanchez.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
   return (
@@ -9,9 +10,9 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/viviansanchez.png" />
+        <Avatar src="https://github.com/pocketbase.png" />
 
-        <strong>Vivian Sanchez</strong>
+        <strong>Pocketbase</strong>
         <span>Web Developer</span>
       </div>
 
