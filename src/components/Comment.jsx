4336b3ee0183227d72eb433b1fea07274be7ee32
Vivian Laptop Nitro <visasal88@gmail.com>
2024-10-15 11:12:18 -0300

    feat: add prop content

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/viviansanchez.png" />
@@ -22,7 +22,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Muito bom Devon, parabéns!</p>
+          <p>{content}</p>
         </div>
 
         <footer>
