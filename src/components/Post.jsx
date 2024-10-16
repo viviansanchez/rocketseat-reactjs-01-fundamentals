cae7d949fc2656cb10abc114e32b30d202dbdd3d
Vivian Laptop Nitro <visasal88@gmail.com>
2024-10-16 09:50:56 -0300

    feat: add function deleteComment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,6 +31,14 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText("");
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
+
+    setComments(commentsWithoutDeletedOne);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -78,7 +86,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment key={comment} content={comment} />;
+          return <Comment key={comment} content={comment} onDeleteComment={deleteComment} />;
         })}
       </div>
     </article>
